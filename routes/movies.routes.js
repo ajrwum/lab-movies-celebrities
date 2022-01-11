22944c90fbeb2ed91206fe3190721aef7d74428a
Ajr Wum <ajr.wum@gmail.com>
2022-01-11 19:19:01 +0100

    iteration 6 & 7 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,5 @@
 const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
@@ -9,7 +10,40 @@ const router = require("express").Router();
 /** PREFIX, set in app.js: ------   /movies   ------ */
 /** ------------------------------------------------ */
 
+// - the route to display all movies
+router.get('/', async (req, res, next) => {
+  try {
+    const movies = await Movie.find();
+    res.render('movies/movies', { movies });
+  } catch (error) {
+    next(error);
+  }
+});
+
 // - the route to display the create form
+router.get('/create', async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find();
+    res.render('movies/new-movie', { celebrities });
+  } catch (error) {
+    
+  }
+});
+
+// - the route to actually create the movie in db
+router.post('/create', async (req, res, next) => {
+  try {
+    // getting the user's input
+    const movie = { ...req.body };
+    // testing if the cast is a string (only 1 id passed) or the expected array
+    if (typeof movie.cast === 'string') movie.cast = [ movie.cast ];
+    await Movie.create(movie);
+    // redirect to all movies page
+    res.redirect('/movies');
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 module.exports = router;
