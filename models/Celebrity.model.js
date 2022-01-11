650e124d84b9b94a93a295718b262cd6469f02e6
Ajr Wum <ajr.wum@gmail.com>
2022-01-11 17:13:48 +0100

    iteration 2 done - add the celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,16 @@
 //  Add your code here
+// loading necessary stuff form mongoose
+const { Schema, model } = require('mongoose');
+
+// creating the schema
+const celebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String
+});
+
+// defining the model
+const Celebrity = model("celebritie", celebritySchema);
+
+// exporting to enable access from anywhere
+module.exports = Celebrity;
